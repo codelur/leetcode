6a91836a1241e56acc1f2641e0b381d14e21fd1c
Sal <codelur@gmail.com>
2025-04-27 14:51:14 -0700

    problem 161 leetcode

--- src/main/java/com/leetcode/problems/DynamicProgramming/ProblemsLeetCode.java
+++ src/main/java/com/leetcode/problems/DynamicProgramming/ProblemsLeetCode.java
@@ -17,6 +17,43 @@ public class ProblemsLeetCode {
              }
          }
 
+    // Problem 161
+    private boolean editRec(String s, String t, int indexS, int indexT, int changes){
+        //end of String
+
+        if(indexS == s.length() && indexT == t.length())
+            return changes == 1? true : false;
+
+        if( (s.length() - indexS == 1 && t.length() - indexT == 0 && changes == 0)
+                ||  (s.length()- indexS == 0 && t.length() - indexT == 1 && changes == 0))
+            return true;
+
+        //end of only one of the strings
+        if ((indexS != s.length() && indexT == t.length())
+                || (indexS == s.length() && indexT != t.length()))
+            return false;
+
+
+        char sC = s.charAt(indexS);
+        char tC = t.charAt(indexT);
+        if(sC == tC){
+           return editRec(s,  t,  indexS +1,  indexT+1,  changes);
+        }else{
+            if(changes == 1)
+                return false;
+            boolean replace = editRec(s,  t,  indexS +1,  indexT+1,  changes + 1);
+            boolean addInT = editRec(s,  t,  indexS ,  indexT+1,  changes + 1);
+            boolean addInS = editRec(s,  t,  indexS+1 ,  indexT,  changes + 1);
+
+            return replace || addInS || addInT;
+        }
+    }
+
+    public boolean isOneEditDistance(String s, String t) {
+        return editRec( s,  t, 0, 0, 0);
+
+    }
+
     //Problem 129
     int sumTotal = 0;
 
