80eab8b17761ec0fec7b32fbb66c9cf6f365e300
Sal <codelur@gmail.com>
2025-04-28 14:38:19 -0700

    problem 792 leetcode

--- src/main/java/com/leetcode/problems/DynamicProgramming/ProblemsLeetCode.java
+++ src/main/java/com/leetcode/problems/DynamicProgramming/ProblemsLeetCode.java
@@ -40,6 +40,34 @@ public class ProblemsLeetCode {
 
     }
 
+    //Problem 174
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j == n - 1) {
+                    // Bottom-right cell (princess)
+                    dp[i][j] = Math.max(1, 1 - dungeon[i][j]);
+                } else if (i == m - 1) {
+                    // Last row (can only go right)
+                    dp[i][j] = Math.max(1, dp[i][j + 1] - dungeon[i][j]);
+                } else if (j == n - 1) {
+                    // Last column (can only go down)
+                    dp[i][j] = Math.max(1, dp[i + 1][j] - dungeon[i][j]);
+                } else {
+                    // Middle cells (choose min between right and down)
+                    int minNext = Math.min(dp[i + 1][j], dp[i][j + 1]);
+                    dp[i][j] = Math.max(1, minNext - dungeon[i][j]);
+                }
+            }
+        }
+
+    return dp[0][0];
+
+    }
+
     //Problem 792
     public int numMatchingSubseq(String s, String[] words) {
         int count = 0;
