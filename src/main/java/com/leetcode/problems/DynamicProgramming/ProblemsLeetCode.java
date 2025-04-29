c45a7f6511db692a97f0f6bdc8a2f8ae7be5cca5
Sal <codelur@gmail.com>
2025-04-28 19:08:49 -0700

    problem 337 leetcode

--- src/main/java/com/leetcode/problems/DynamicProgramming/ProblemsLeetCode.java
+++ src/main/java/com/leetcode/problems/DynamicProgramming/ProblemsLeetCode.java
@@ -40,6 +40,26 @@ public class ProblemsLeetCode {
 
     }
 
+    //Problem 337
+    private int[] robHouse(TreeNode root) {
+        if (root == null) return new int[]{0,0};
+
+        int[] left = robHouse(root.left);
+        int[] right = robHouse(root.right);
+
+        int rob = root.val + left[0] + right[0];
+
+        int notRob = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+
+        return new int[]{notRob, rob};
+    }
+
+    public int rob(TreeNode root) {
+        int[] result = robHouse(root);
+        return Math.max(result[0], result[1]);
+    }
+
     //Problem 174
     public int calculateMinimumHP(int[][] dungeon) {
         int m = dungeon.length;
