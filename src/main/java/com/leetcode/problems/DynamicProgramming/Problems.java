1d2862eb3b527ee5c533516a574a25e07f5b1fa5
Sal <codelur@gmail.com>
2025-05-09 17:26:44 -0700

    problem 787 leetcode

--- src/main/java/com/leetcode/problems/DynamicProgramming/Problems.java
+++ src/main/java/com/leetcode/problems/DynamicProgramming/Problems.java
@@ -72,6 +72,28 @@ public class Problems {
         return prev[dst] == Integer.MAX_VALUE? -1 : prev[dst];
     }
 
+    //Problem 787
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] prev = new int[n];
+        Arrays.fill(prev, Integer.MAX_VALUE);
+        prev[src] = 0;
+
+        for (int i=0;i<k+1;i++){
+            int[] curr = Arrays.copyOf(prev, n);
+            for(int[] flight: flights){
+                int from = flight[0];
+                int to = flight[1];
+                int price = flight[2];
+                if(prev[from]!=Integer.MAX_VALUE){
+                    curr[to] = Math.min(curr[to], prev[from] + price);
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[dst] == Integer.MAX_VALUE? -1 : prev[dst];
+    }
+
     //Problem 418
     public int wordsTyping(String[] sentence, int rows, int cols) {
 
